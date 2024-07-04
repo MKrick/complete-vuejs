149613fab1b06c01b153bd1650957ec7fd1e157e
Matthew Krick <krick.matthew@gmail.com>
2024-07-03 17:23:32 -0700

    section 02

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import * as Vue from 'vue/dist/vue.esm-bundler.js';
+
+const Num = {
+  props: ['number'],
+  template: `
+  <button
+    v-bind:class="getClass(number)"
+    v-on:click="click(number)"
+  >
+    {{ number }}
+  </button>
+  `,
+
+  methods: {
+    click(number) {
+      this.$emit('chosen', this.number)
+    },
+    getClass(number) {
+      return this.isEven(number) ? 'blue' : 'red'
+    },
+    isEven(number) {
+      return number % 2 === 0
+    }
+
+  }
+}
+
+const app = Vue.createApp({
+  components: {
+    Num
+  },
+
+  template: `
+  <num
+    v-for="number in numbers"
+    v-bind:number="number"
+    v-on:chosen="addNumber"
+  />
+
+  <hr />
+
+  <num
+    v-for="number in numberHistory"
+    v-bind:number="number"
+  />
+
+  <hr />
+
+  {{ numberHistory }}
+  `,
+
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      numberHistory: []
+    }
+  },
+
+  computed: {
+    evenList() {
+      return this.numbers.filter(num => this.isEven(num))
+    }
+  },
+
+  methods: {
+    addNumber(number) {
+      this.numberHistory.push(number)
+    }
+  }
+})
+app.mount('#app')
