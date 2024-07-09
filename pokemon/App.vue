0646445f845aa652b2a043ab4e5b6b08e6da39d6
Matthew Krick <krick.matthew@gmail.com>
2024-07-08 21:09:24 -0700

    section 03 completed

--- pokemon/App.vue
+++ pokemon/App.vue
@@ -1,24 +1,47 @@
 <template>
-  <div class="card">
-    <div class="title">Title</div>
-    <div class="content">Content</div>
-    <div class="description">Description</div>
-    <button @click="fetchData">Fetch</button>
-  </div>
+  <pokemon-cards
+    :pokemons="pokemons"
+    @chosen="fetchEvolutions"
+    :selectedId="selectedId"
+  />
+
+  <pokemon-cards :pokemons="evolutions" />
 </template>
 
 <script>
+
+import PokemonCards from './PokemonCards.vue';
+
 const api = 'https://pokeapi.co/api/v2/pokemon'
-const ids = [1, 4, 7]
+const IDS = [1, 4, 7]
+
 export default {
+  components: {
+    PokemonCards
+  },
+
   data() {
     return {
-      pokemon: []
+      pokemons: [],
+      evolutions: [],
+      selectedId: null
     }
   },
 
+  async created() {
+    this.pokemons = await this.fetchData(IDS)
+  },
+
+
   methods: {
-    async fetchData() {
+    async fetchEvolutions(pokemon) {
+      this.evolutions = await this.fetchData(
+        [pokemon.id + 1, pokemon.id + 2]
+      )
+      this.selectedId = pokemon.id
+    },
+
+    async fetchData(ids) {
       const responses = await Promise.all(
         ids.map(id => window.fetch(`${api}/${id}`))
       )
@@ -26,46 +49,16 @@ export default {
         responses.map(data => data.json())
       )
       
-      this.pokemon = json.map(datum => ({
+      return json.map(datum => ({
         id: datum.id,
         name: datum.name,
         sprite: datum.sprites.other['official-artwork'].front_default,
         types: datum.types.map(type => type.type.name)
       }))
-
-      console.log(this.pokemon)
     }
   }
 }
 </script>
 
 <style scoped>
-.card {
-  border: 1px solid silver;
-  border-radius: 8px;
-  max-width: 200px;
-  margin: 0 5px;
-  cursor: pointer;
-  box-shadow: 0px 1px 3px darkgrey;
-  transition: 0.2s;
-}
-
-.card:hover {
-  transition: 0.2s;
-  box-shadow: 0px 1px 9px darkgrey;
-}
-
-.title, .content, .description {
-  padding: 16px;
-  text-transform: capitalize;
-  text-align: center;
-}
-
-.title, .content {
-  border-bottom: 1px solid silver;
-}
-
-.title {
-  font-size: 1.25em;
-}
 </style>
